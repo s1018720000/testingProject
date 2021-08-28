9fd2aaaca76f41ef704f03827dfd92e86b3759f0
kolin.z <kolin.z@my-cpg.com>
2021-08-28 21:29:32 +0800

    fix some issue

--- as-quartz/src/main/java/com/as/quartz/util/OkHttpUtils.java
+++ as-quartz/src/main/java/com/as/quartz/util/OkHttpUtils.java
@@ -24,7 +24,8 @@ public class OkHttpUtils {
                             .readTimeout(60, TimeUnit.SECONDS)
                             .writeTimeout(120, TimeUnit.SECONDS)
                             .connectionPool(new ConnectionPool(50, 5, TimeUnit.MINUTES))
-                            .retryOnConnectionFailure(true)
+                            .retryOnConnectionFailure(false)
+                            .dns(new OkHttpDns(60))
                             .build();
                 }
             }
